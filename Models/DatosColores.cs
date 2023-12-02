83d1c43c4125fb12f2a801e10830d3a61f771d00
JuniorCalvo <JuniorCalvo0410@gmail.com>
2023-12-01 22:10:02 -0400

    Agregar archivos de proyecto.

---
+++ Models/DatosColores.cs
@@ -0,0 +1,14 @@
+﻿namespace Colorinweb.Models
+{
+    public class DatosColores
+    {
+        public int Rojo { get; set; }
+        public int Verde { get; set; }
+        public int Azul { get; set; }
+
+        public DatosColores()
+        {
+            // Constructor sin parámetros
+        }
+    }
+}
---
+++ Servicio/ColorService.cs
@@ -0,0 +1,63 @@
+﻿// ColorService.cs
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Colorinweb.Models;
+using Newtonsoft.Json;
+
+namespace Colorinweb.Servicio
+{
+    public class ColorService
+    {
+        private readonly string folderPath = "datax";
+
+        public async Task<DatosColores> CargarDatosAsync(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var datosJson = await File.ReadAllTextAsync(filePath);
+                    return JsonConvert.DeserializeObject<DatosColores>(datosJson) ?? new DatosColores();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al cargar datos: {ex.Message}");
+            }
+
+            return new DatosColores();
+        }
+
+        public void GuardarDatos(DatosColores datos, string filePath)
+        {
+            try
+            {
+                var datosJson = JsonConvert.SerializeObject(datos);
+                File.WriteAllText(filePath, datosJson);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al guardar datos: {ex.Message}");
+            }
+        }
+
+        public DatosColores CargarDatos(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var datosJson = File.ReadAllText(filePath);
+                    return JsonConvert.DeserializeObject<DatosColores>(datosJson) ?? new DatosColores();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al cargar datos: {ex.Message}");
+            }
+
+            return new DatosColores();
+        }
+    }
+}
